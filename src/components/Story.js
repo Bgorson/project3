4e08159509cd30568079989ceeddd4ae42fd26ea
Jessica Chen <jchen@dhcp-10-101-136-235.wireless.northwestern.private>
2019-05-06 18:34:18 -0500

    added img

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import Island from '../images/island.svg'
 
 class Story extends Component {
+    render() {
+        return (
+            <div className="container">
+                <img src={Island} className="island" alt="island"/>
 
+                <Typography  className="story-content" component="p">
+                    You had a great day at school and you’re in a rush to return home to hang out 
+                    with your faithful companion (pet name). To save on time, you decide to take a shortcut 
+                    through Kingsthon Hallow, even though it’s against your parents’ wishes. How hard could 
+                    it be? It wasn’t long before you got lost. You look around but all you could see were 
+                    trees. Nervously, you felt into your bag for your favorite toy (pet name), but (pet name) 
+                    was nowhere to be found! You begin to panic. You are sure you packed (pet name). 
+                    To make matters worse, you begin hear the growls of a wild animal nearby. You begin 
+                    to run through Kingthon Hallow looking for any sanctuary you can find! Luckily, you 
+                    stumble upon a cave in the forest. This is where your journey begins . . .  
+                </Typography>
+            </div>
+        )
+    }
 }
 
 export default Story;
