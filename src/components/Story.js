9df34953851b0dde05a3450633a7addd70bb2972
Jessica Chen <jchen@dhcp-10-101-136-235.wireless.northwestern.private>
2019-05-06 19:08:53 -0500

    added cont btn

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import Island from '../images/island.svg'
 
@@ -19,6 +22,15 @@ class Story extends Component {
                     To make matters worse, you begin hear the growls of a wild animal nearby. You begin 
                     to run through Kingthon Hallow looking for any sanctuary you can find! Luckily, you 
                     stumble upon a cave in the forest. This is where your journey begins . . .  
+                    <Button 
+                        variant="contained" 
+                        color="primary">    
+                            <Link 
+                                to="/main" 
+                                className="btn btn-link">
+                                CONT.
+                            </Link>
+                    </Button> 
                 </Typography>
             </div>
         )
