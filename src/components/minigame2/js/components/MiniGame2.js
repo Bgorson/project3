6178f2d5b0a45092673ce8c92cd1728156853754
Gastonm1 <gaston.marques@gmail.com>
2019-05-07 21:17:30 -0500

    grid layout

--- src/components/minigame2/js/components/MiniGame2.js
+++ src/components/minigame2/js/components/MiniGame2.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../../css/stylesheet.css";
 import { Motion, spring } from "react-motion";
 
+const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""];
 
-const cells= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0];
-
-const array = [
-  [1,2,3,4],
-  [5,6,7,8],
-  [9,'',11,12],
-  [13,14,15,10]
-]; // Keep track of empty cell by using the 'state'
-
+const array = [[1, 2, 3, 4], [5, 6, 7, 8], [9, "", 11, 12], [13, 14, 15, 10]]; // Keep track of empty cell by using the 'state'
 
 class MiniGame2 extends Component {
-  state = {
-    
-  }
+  state = {};
 
- handleClick = () => {
-   console.log("click")
- }
+  handleClick = () => {
+    console.log("click");
+  };
 
   render() {
     return (
-      <div className= "container">
-        {
-          cells.map( (cell, i) => {
-            return (<div key= {i} onClick= {this.handleClick}>{cell}</div>)
-          })
-        }
+      <div className="puzzle">
+        {cells.map((cell, i) => {
+          return (
+            <div key={i} onClick={this.handleClick}>
+              {cell}
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
