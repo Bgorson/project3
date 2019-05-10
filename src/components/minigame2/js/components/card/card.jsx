b57818d9e7f2f6708637712f069534a526da3f75
Gastonm1 <gaston.marques@gmail.com>
2019-05-10 12:47:01 -0500

    card JSX

--- src/components/minigame2/js/components/card/card.jsx
+++ src/components/minigame2/js/components/card/card.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactCardFlip from 'react-card-flip';
 
-const Card ({ id, isFlipped, handleClick, cardNumber }) => (
-    <ReactCardFlip is Flipped = {isFlipped={isFlipped} flipSpeedBackToFront={2} flilpSpeedFrontToBack{2}>
+const Card = ({ id, isFlipped, handleClick, cardNumber }) => (
+    <ReactCardFlip is Flipped = {isFlipped} flipSpeedBackToFront={2} flipSpeedFrontToBack={2}>
     <button id={id} className={`card card-front ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key ="front"> 
     </button>
 
-    
-    </ReactCardFlip>}
-)
+    <button id={id} className={`card card-back ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key ="back"> 
+    </button>
+    </ReactCardFlip>
+);
+
+export default Card;
