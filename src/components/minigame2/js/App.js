6ada9f7d481d3d524a5063b96fda2a30a573327d
Gastonm1 <gaston.marques@gmail.com>
2019-05-11 00:10:22 -0500

    cards

--- src/components/minigame2/js/App.js
+++ src/components/minigame2/js/App.js
@@ -1,13 +1,11 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from "react";
 // import MiniGame2 from "./components/MiniGame2";
-import Header from './components/header/Header'
-import Card from './components/card/card'
-import GameOver from './components/card/GameOver'
+import Header from "./components/header/Header";
+import Card from "./components/card/card";
+import GameOver from "./components/card/GameOver";
 import "../css/stylesheet.css";
 
-
 class App extends PureComponent {
-
   state = {
     isFlipped: Array(16).fill(false),
     shuffleCard: App.duplicateCard.sort(() => Math.random() - 0.5),
@@ -16,26 +14,43 @@ class App extends PureComponent {
     prevCardID: -1
   };
 
-  static duplicateCard =  () => {
-    return [0,1,2,3,4,5,6,7].reduce((preValue, current, index, array) => {
-      return preValue.concat([current, current])
-      // console.log(current)
-    },[])
-    };
-    
-    handleClick = event => {
-      event.preventDefault();
-      const cardID = event.target.id;
-      const newFlipps = this.state.isFlipped.slice();
-      this.setState({
-        prevSelectedCard: this.state.shuffledCard[cardId],
-        prevCardID: cardId
-      });
+  static duplicateCard = () => {
+    return [0, 1, 2, 3, 4, 5, 6, 7].reduce(
+      (preValue, current, index, array) => {
+        return preValue.concat([current, current]);
+        // console.log(current)
+      },
+      []
+    );
+  };
 
+  handleClick = event => {
+    event.preventDefault();
+    const cardID = event.target.id;
+    const newFlipps = this.state.isFlipped.slice();
+    this.setState({
+      prevSelectedCard: this.state.shuffledCard[cardId],
+      prevCardID: cardId
+    });
+
+    if (newFlipps[cardId] === false) {
+      newFlipps[cardId] = !newFlipps[cardId];
+      this.setState(prevState => ({
+        isFlipped: newFlipps,
+        clickCount: this.state.clickCount + 1
+      }));
+
+      if (this.state.clickCount === 2) {
+        // console.log(this.state.clickCount)
+        this.setState({ clickCount: 1 });
+        const prevCardId = this.state.prevCardId;
+        const newCard = this.state.shuffledCard[cardId];
+        const previousCard = this.state.prevSelectedCard;
+
+        this.isCardMatch(previousCard, newCard, prevCardId, cardId);
+      }
     }
-    
-    )
-  }
+  };
 }
-   
+
 export default App;
