9c0f69c06e407089d166f179f678daa1bb647a99
Gastonm1 <gaston.marques@gmail.com>
2019-05-10 23:05:25 -0500

    app.js

--- src/components/minigame2/js/App.js
+++ src/components/minigame2/js/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import MiniGame2 from "./components/MiniGame2";
+import Header from './components/header/Header'
+import Card from './components/card/card'
+import GameOver from './components/card/GameOver'
 import "../css/stylesheet.css";
-import "../css/game.css";
-import "../css/cell.css";
+
 
 function App() {
   return <MiniGame2/>
