a9662b378707030145e7b82892188ecdb28c2bc4
Gastonm1 <gaston.marques@gmail.com>
2019-05-10 23:38:38 -0500

    state manipulation

--- src/components/minigame2/js/App.js
+++ src/components/minigame2/js/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import MiniGame2 from "./components/MiniGame2";
+import React, {PureComponent} from 'react';
+// import MiniGame2 from "./components/MiniGame2";
 import Header from './components/header/Header'
 import Card from './components/card/card'
 import GameOver from './components/card/GameOver'
 import "../css/stylesheet.css";
 
 
-function App() {
+class App extends PureComponent {
+
+  state = {
+    isFlipped: Array(16).fill(false),
+    shuffleCard: App.duplicateCard.sort(() => Math.random() - 0.5),
+    clickCount: 1,
+    prevSelectedCard: -1,
+    prevCardID: -1
+  };
+
   return <MiniGame2/>
 }
    
