bdd32f2d26ff0c44a7bbc160f706361b9177a092
Jessica Chen <jchen@dhcp-10-101-136-235.wireless.northwestern.private>
2019-05-13 19:46:45 -0500

    adjusted size and color

--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 import Island from '../images/island.svg'
+
+import './story.css';
 
 class Story extends Component {
     componentDidMount() {
@@ -14,8 +15,7 @@ class Story extends Component {
         return (
             <div className="container">
                 <img src={Island} className="island" alt="island"/>
-
-                <Typography  className="story-content" component="p">
+                <div  className="story-content">
                     You had a great day at school and you’re in a rush to return home to hang out 
                     with your faithful companion {this.props.petname}. To save on time, you decide to take a shortcut 
                     through Kingsthon Hallow, even though it’s against your parents’ wishes. How hard could 
@@ -25,16 +25,19 @@ class Story extends Component {
                     To make matters worse, you begin hear the growls of a wild animal nearby. You begin 
                     to run through Kingthon Hallow looking for any sanctuary you can find! Luckily, you 
                     stumble upon a cave in the forest. This is where your journey begins . . .  
-                    <Button 
+                <br />
+                <div className="storyBtn">
+                <Button 
                         variant="contained" 
                         color="primary">    
                             <Link 
                                 to="/main" 
                                 className="btn btn-link">
-                                CONT.
+                                CONTINUE
                             </Link>
                     </Button> 
-                </Typography>
+                    </div>
+                </div>
             </div>
         )
     }
